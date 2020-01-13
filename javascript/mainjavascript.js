5f0ad7c42b82129a24444e9e5df7a1d56c088f57
ArgEric8 <ericnilden@gmail.com>
2020-01-13 21:44:23 +0100

    javascript

--- javascript/mainjavascript.js
+++ javascript/mainjavascript.js
@@ -1,22 +1,37 @@
-    
-    /* https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_self */
-    var slideNumber = 1;
-    displaySlide(slideNumber);
 
-    function changeSlide(n) {
-        displaySlide (slideNumber += n);
+/* https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_self */
+var slideNumber = 1;
+displaySlide(slideNumber);
+
+function changeSlide(n) {
+    displaySlide (slideNumber += n);
+}
+
+function displaySlide(n) {
+    var i;
+    var x = document.getElementsByClassName("slideImage");
+    // Loop around
+    if (n > x.length) {slideNumber = 1}
+    if (n < 1) {slideNumber = x.length}
+
+    for (i = 0; i < x.length; i++)
+    {
+        x[i].style.display = "none";
     }
+    x[slideNumber-1].style.display = "flex";
+}
 
-    function displaySlide(n) {
-        var i;
-        var x = document.getElementsByClassName("slideImage");
-        // Loop around
-        if (n > x.length) {slideNumber = 1}
-        if (n < 1) {slideNumber = x.length}
+/* https://www.w3schools.com/howto/howto_js_popup_form.asp */
 
-        for (i = 0; i < x.length; i++)
-        {
-            x[i].style.display = "none";
-        }
-        x[slideNumber-1].style.display = "flex";
+function showMobileMenu() {
+    document.getElementById("popUp--navBar").style.display="flex";
+    document.getElementById("popUp--navBar").classList.add="popUp--navBar--open";
+    document.getElementById("btn--open").style.display="none";
+    document.getElementById("btn--close").style.display="flex";
     }
+function closeMobileMenu() {
+    document.getElementById("popUp--navBar").style.display="none";
+    document.getElementById("popUp--navBar--open").classList.remove="popUp--navBar--open";
+    document.getElementById("btn--close").style.display="none";
+    document.getElementById("btn--open").style.display="flex";
+}
